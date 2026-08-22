62da7575655ca81e4b7070f76d98292dbecfb675
maryamshkk <maryamshk117@gmail.com>
2026-08-23 01:52:12 +0500

    tailwindcss fixes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,11 @@
 function Home(){
-    return <h1 className="text-3xl font-bold"
-    >Timero home</h1>
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <h1 className="text-4xl font-bold">
+                Timeora
+            </h1>
+        </div>
+    )
 }
 
-export default Home
+export default Home;
