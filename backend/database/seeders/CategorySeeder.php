abe526a3116a22dcc32f371727ddcc1454982fa5
maryamshkk <maryamshk117@gmail.com>
2026-08-24 22:37:57 +0500

    Updated bugs in Category-Seeder

--- backend/database/seeders/CategorySeeder.php
+++ backend/database/seeders/CategorySeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class CategorySeeder extends Seeder
 {
@@ -33,8 +34,7 @@ class CategorySeeder extends Seeder
                 [
                     'slug' => Str::slug($name),
                     'status' => 'active',
-                ]
-               );
+                ]);
             }
         
     }
