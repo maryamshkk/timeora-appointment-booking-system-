00501476c3a7434bc616097691c76fdd35ed5e55
maryamshkk <maryamshk117@gmail.com>
2026-08-24 21:59:52 +0500

    company admin model

---
+++ backend/app/Models/CompanyAdmin.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+
+
+class CompanyAdmin extends Model
+{
+    //
+
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [ 
+        'company_id',
+        'name',
+        'email',
+        'password_hash',
+        'email_verified_at',
+        'status',
+    ];
+    protected $hidden = [
+        'password_hash',
+    ];
+    protected $casts = [
+        'email_verified_at' =>'datetime',
+    ];
+
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function otps(): MorphMany
+    {
+        return $this->morphMany(Otp::class);
+    }
+
+    public function auditLogs(): MorphMany
+    {
+        return $this->morphMany(AuditLog::class);
+    }
+    public function getAuthpassword()
+    {
+        return $this->password_hash; 
+    }
+}
+
