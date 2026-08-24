8aed76a02c08eb6d223e6581cb4988db7576880b
maryamshkk <maryamshk117@gmail.com>
2026-08-24 19:28:09 +0500

    otps table migrated

--- backend/database/migrations/2026_08_24_140916_create_otps_table.php
+++ backend/database/migrations/2026_08_24_140916_create_otps_table.php
@@ -13,7 +13,26 @@ return new class extends Migration
     {
         Schema::create('otps', function (Blueprint $table) {
             $table->id();
+
+            $table->enum('owner_type', [
+                'company_admin',
+                'customer'
+            ]);
+
+            $table->UnsignedBigInteger('owner_id');
+
+            $table->char('code', 6);
+            $table->enum('purpose', [
+                    'email_verification',
+                    'password_reset']);
+
+            $table->unsignedTinyInteger('attempts')->default(0);
+            $table->timestamp('expires_at');
+            $table->timestamp('verified_at');
             $table->timestamps();
+
+            $table->index(['owner_type', 'owner_id']);
+            $table->index(['owner_type', 'owner_id', 'purpose']);
         });
     }
 
