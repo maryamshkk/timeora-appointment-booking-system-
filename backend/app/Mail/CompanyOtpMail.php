547063452c7d1b7a1755828902158bcd78743ccc
maryamshkk <maryamshk117@gmail.com>
2026-08-26 15:09:54 +0500

    mailable creation

---
+++ backend/app/Mail/CompanyOtpMail.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class CompanyOtpMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function build(){
+        return $this
+        ->subject('TIMEORA Email Verification Code');
+        ->view('emails.company-otp')
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Company Otp Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'view.name',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
