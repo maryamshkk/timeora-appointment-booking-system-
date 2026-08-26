a804368ed042b9ce49287d039e0931feb72968e3
maryamshkk <maryamshk117@gmail.com>
2026-08-26 21:23:29 +0500

    changes in model, controller in process

--- backend/app/Models/Customer.php
+++ backend/app/Models/Customer.php
@@ -3,8 +3,36 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Customer extends Model
+
+class Customer extends Auhenticatable
 {
-    //
+        use HasApiTokens, Notifiable;
+
+        protected $table = 'customers';
+
+        protected $fillable = [
+            'name',
+            'email',
+            'phone',
+            'password_hash',
+            'profile_image',
+            'email_verified_at',
+            'status',
+        ];
+
+        protected $hidden = [
+            'password_hash',
+        ];
+
+        protected $casts = [
+            'email_verified_at' => 'datetime',
+        ];
+
+        public function getAuthPassword()
+        {
+            return $this->password_hash;
+        }
+
 }
