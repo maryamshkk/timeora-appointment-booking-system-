13ae9fabe4c9ade02f8da304c32382793346cf0c
maryamshkk <maryamshk117@gmail.com>
2026-08-27 22:59:21 +0500

    roles migration completed

--- backend/database/migrations/2026_08_27_174232_create_roles_table.php
+++ backend/database/migrations/2026_08_27_174232_create_roles_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name', 100);
+
+            $table->foreignId('company_id')
+                    ->nullable()
+                    ->contrained('companies')
+                    ->cascadeOnDelete();
+                    
             $table->timestamps();
         });
     }
