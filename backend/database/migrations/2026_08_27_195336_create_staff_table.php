fb1fa372b0168c0d5d826051afc44822fa4b5d52
maryamshkk <maryamshk117@gmail.com>
2026-08-28 01:12:34 +0500

    staff table modified

--- backend/database/migrations/2026_08_27_195336_create_staff_table.php
+++ backend/database/migrations/2026_08_27_195336_create_staff_table.php
@@ -10,53 +10,53 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('staff', function (Blueprint $table) {
-            $table->id();
+    // {
+    //     Schema::create('staff', function (Blueprint $table) {
+    //         $table->id();
 
-            $table->string('staff_id', 20)->unique();
+    //         $table->string('staff_id', 20)->unique();
 
-            $table->foreignId('company_id')
-                    ->constrained('companies')
-                    ->cascadeOnDelete();
+    //         $table->foreignId('company_id')
+    //                 ->constrained('companies')
+    //                 ->cascadeOnDelete();
 
-            $table->string('first_name', 100);
-            $table->string('last_name', 100);
+    //         $table->string('first_name', 100);
+    //         $table->string('last_name', 100);
 
-            $table->string('phone', 30)->nullable()->unique();
-            $table->string('account_email', 150)->unique();
+    //         $table->string('phone', 30)->nullable()->unique();
+    //         $table->string('account_email', 150)->unique();
 
-            $table->string('password_hash', 255)->nullable()->unique();
+    //         $table->string('password_hash', 255)->nullable()->unique();
 
-            $table->string('photo_path', 255)->nullable();
+    //         $table->string('photo_path', 255)->nullable();
 
-            $table->foreignId('role_id')
-                ->constrained('roles')
-                ->restrictOnDelete();
+    //         $table->foreignId('role_id')
+    //             ->constrained('roles')
+    //             ->restrictOnDelete();
 
-            $table->enum('invitation_status', [
-                'not_sent',
-                'pending',
-                'accepted'
-            ])->default('not_sent');
+    //         $table->enum('invitation_status', [
+    //             'not_sent',
+    //             'pending',
+    //             'accepted'
+    //         ])->default('not_sent');
 
-            $table->string('invitation_token', 255)->nullable();
+    //         $table->string('invitation_token', 255)->nullable();
 
-            $table->timestamp('invitation_sent_at')->nullable();
+    //         $table->timestamp('invitation_sent_at')->nullable();
 
-            $table->timestamp('email_verified_at')->nullable();
+    //         $table->timestamp('email_verified_at')->nullable();
             
-            $table->enum('status', [
-                'active',
-                'pending',
-                'deactivated'
-            ])->default('pending');
+    //         $table->enum('status', [
+    //             'active',
+    //             'pending',
+    //             'deactivated'
+    //         ])->default('pending');
 
-            $table->boolean('is_active')->default(true);
+    //         $table->boolean('is_active')->default(true);
 
-            $table->timestamps();
-        });
-    }
+    //         $table->timestamps();
+    //     });
+    // }
 
     /**
      * Reverse the migrations.
