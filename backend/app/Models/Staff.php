7fb1c3e6021f7596368c53d9a1a2fac93f0989c6
maryamshkk <maryamshk117@gmail.com>
2026-08-28 17:43:59 +0500

    bioo feild add in staff table

--- backend/app/Models/Staff.php
+++ backend/app/Models/Staff.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Staff extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'company_id',
+        'staff_id',
+        'first_name',
+        'last_name',
+        'photo_path',
+        'role_id',
+        'phone',
+        'account_email',
+        'password_hash',
+        'title',
+        'bio',
+        'experience_years',
+        'rating_avg',
+        'invitation_status',
+        'invitation_token',
+        'invitation_sent_at',
+        'invitation_accepted_at',
+        'email_verified_at',
+        'status',
+        'is_active',
+    ];
 }
