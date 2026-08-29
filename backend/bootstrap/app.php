c8823cc3297cc78bf8a5eea51513e2311d22af22
maryamshkk <maryamshk117@gmail.com>
2026-08-29 15:44:06 +0500

    authorization with middleware specified

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Auth\AuthenticationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,17 +14,27 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
 
-
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
+
     })
 
-
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->shouldRenderJsonWhen(
-            fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
-        );
+
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Unauthenticated.',
+                    'data' => null,
+                    'errors' => null,
+                ], 401);
+            }
+
+        });
+
     })->create();
