066e7b4a3fcbadb70c4e908d180816f6e1d60c95
maryamshkk <maryamshk117@gmail.com>
2026-08-29 20:24:27 +0500

    get company data api

---
+++ backend/app/Http/Controllers/CompanyController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CompanyController extends Controller
+{
+    //GET COMPANY
+    public function show(Request $request)
+    {
+        $user = $request->user();
+
+        $company = $request->company();
+
+        if(!$company)
+            {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Company not found',
+                    'data' => null,
+                    'errors' => null,
+                ], 404);
+            }
+        
+            return response()->json([
+                'success' => true,
+                'message' => 'Company fetched successfully',
+                'data' => [
+                    'company' => $company,
+                ],
+                'errors' => null,
+            ], 200);
+    }
+    
+}
