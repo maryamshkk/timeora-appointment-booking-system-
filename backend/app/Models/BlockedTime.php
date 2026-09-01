85615ac67d3abc7a91193106a4bea3e0dd024a4d
maryamshkk <maryamshk117@gmail.com>
2026-09-01 16:20:58 +0500

    Blocked time schedule model created

---
+++ backend/app/Models/BlockedTime.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class BlockedTime extends Model
+{
+    protected $fillable = [
+        'staff_id',
+        'blocked_date',
+        'start_time',
+        'end_time',
+        'reason'
+    ];
+
+    protected $casts = [
+        'blocked_date' => 'date'
+    ];
+
+    public function staff(): BelongstO
+    {
+        return $this->belongsTo(Staff::class);
+    }
+}
