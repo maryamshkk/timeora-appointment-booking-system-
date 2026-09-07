1f9daa1dd2b187637ce048fe2d04f99d1e2b6d26
maryamshkk <maryamshk117@gmail.com>
2026-09-07 07:51:23 +0500

    icon box created

---
+++ frontend/src/components/common/ui/IconBox.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function IconBox({children}) {
+
+    return(
+        <div 
+        className="
+            flex
+            h-12
+            w-12
+            items-center
+            justify-center
+            rounded-xl
+            bg-beige
+            text-navy
+        ">
+
+        </div>
+    )
+}
+export default IconBox;
